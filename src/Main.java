ee1437fe73572a434c88464114e05620073d3749
Rafael Filho <rafael.antoniofilho@hotmail.com>
2025-10-01 09:07:18 -0300

    Correção de texto

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
             System.out.println();
             System.out.printf("Record: %d years old, code %d and gender: %s%n", age, code, gender);
             System.out.println();
-            System.out.printf("Measue with eight decimal places: %.8f%n", measure);
+            System.out.printf("Measure with eight decimal places: %.8f%n", measure);
             System.out.printf("Rounded (three decimal places): %.3f%n", measure);
             Locale.setDefault(Locale.US);
             System.out.printf("US decimal point: %.3f", measure);
