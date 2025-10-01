2961981df2c26e53a26b564228498c2d05652e86
Rafael Filho <rafael.antoniofilho@hotmail.com>
2025-10-01 10:14:58 -0300

    Primeira leva de exercicios

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,31 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            String product1 = "Computer";
-            String product2 = "Office desk";
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-            int age = 30;
-            int code = 5290;
-            char gender = 'F';
+        double A, B, C, triangulo, trapezio, retangulo, circulo, quadrado;
 
-            double price1 = 2100.0;
-            double price2 = 650.50;
-            double measure = 53.234567;
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+        C = sc.nextDouble();
+
+        triangulo = A * C / 2;
+        trapezio = (A + B) * C / 2;
+        retangulo = A * B;
+        circulo = (Math.pow(C,2)) * 3.14159;
+        quadrado = B*B;
+
+        System.out.printf("AREA DO TRIANGULO = %.3f%n", triangulo);
+        System.out.printf("AREA DO CIRCULO = %.3f%n", circulo);
+        System.out.printf("AREA DO TRAPEZIO = %.3f%n", trapezio);
+        System.out.printf("AREA DO QUADRADO = %.3f%n", quadrado);
+        System.out.printf("AREA DO RETANGULO = %.3f%n", retangulo);
 
 
-            System.out.println("Product:");
-            System.out.printf("%s, which price is $ %.2f%n", product1, price1);
-            System.out.printf("%s, which price is $ %.2f%n", product2, price2);
-            System.out.println();
-            System.out.printf("Record: %d years old, code %d and gender: %s%n", age, code, gender);
-            System.out.println();
-            System.out.printf("Measure with eight decimal places: %.8f%n", measure);
-            System.out.printf("Rounded (three decimal places): %.3f%n", measure);
-            Locale.setDefault(Locale.US);
-            System.out.printf("US decimal point: %.3f", measure);
-
-        }
+        sc.close();
+    }
 }
